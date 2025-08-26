e378139cd6e1ec376dab9fa7d6d1a50fa9f66d34
winmonn <winmon19@gmail.com>
2025-08-26 14:57:28 +0800

    v1

---
+++ premid/arraylist/ListV1.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX 7
+
+typedef struct {
+    int elem[MAX];
+    int count;
+} List;
+
+List initialize(List L);
+List insertPos(List L, int data, int position);
+List deletePos(List L, int position);
+int locate(List L, int data);
+List insertSorted(List L, int data);
+void display(List L);
+
+int main(){
+    List L = initialize(L);
+	L = insertPos(L, 7, 0);
+	L = insertPos(L, 14, 1);
+	L = insertPos(L, 21, 2);
+	L = insertPos(L, 28, 3);
+	L = insertPos(L, 35, 4);
+	L = insertPos(L, 17, 2);
+	L = deletePos(L, 2);
+	
+	int pos = locate(L, 14);
+	printf("Position: %d\n", pos);
+}
+
+List initialize(List L){
+    L.count = 0;
+
+    return L;
+}
+
+List insertPos(List L, int data, int position){
+	if(position <= MAX - 1){
+		if(L.count < MAX){
+			int i, j;
+			for(i = L.count - 1, j = L.count; j > position ;i--, j--){
+				L.elem[j] = L.elem[i];
+			}
+			(L.count)++;
+			L.elem[position] = data;
+		} else {
+			printf("List is full.\n");
+		}
+	} else {
+		printf("Invalid position.\n");
+	}
+	
+	return L;
+}
+
+List deletePos(List L, int position){
+	if(position <= MAX - 1){
+		if(L.count > 0){
+			int i, j;
+			for(i = position, j = position + 1; j < L.count; i++, j++){
+				L.elem[i] = L.elem[j];
+			}
+			(L.count)--;
+		} else {
+			printf("List is empty.\n");
+		}	
+	} else {
+		printf("Position is invalid.\n");
+	}
+	
+	return L;
+}
+
+int locate(List L, int data){
+	int position = 0;
+	int i;
+	for(i = 0; i < L.count && L.elem[i] != data; i++){}
+	return (i < L.count) ? i : -1;
+}
+
+List insertSorted(List L, int data){
+	
+}
